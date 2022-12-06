003ec9e20f35924e029d65a7f7f09ea1a5b4f6f0
elebur <elebur249@proton.me>
2022-12-06 17:23:58 +0200

    add token file path and export fields settings

--- scrapy_google_sheets_example/settings.py
+++ scrapy_google_sheets_example/settings.py
@@ -1,4 +1,8 @@
+import os
+
 from .pipelines import GoogleSheetsPipeline
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 # Scrapy settings for scrapy_google_sheets_example project
 BOT_NAME = 'scrapy_google_sheets_example'
@@ -7,10 +11,16 @@ NEWSPIDER_MODULE = 'scrapy_google_sheets_example.spiders'
 
 # id of the spreadsheet you want to use to store your data
 GOOGLE_SHEET_ID = '1eX8ftT1jKY2MyUcaHFV-Oo93qGP1NgHdcC-4MFm6UUc'
-# range in which to insert the data
-# the following specifies column A to column B
-GOOGLE_SHEET_RANGE = 'A:B'
 
+# Absolut path to the pickled file with auth data
+GOOGLE_SHEET_TOKEN_FILEPATH = os.path.join(PROJECT_ROOT, "resources", "token.pickle")
+
+# Fields to export to the sheet.
+# This settings has higher priority than FEED_EXPORT_FIELDS
+GOOGLE_SHEET_EXPORT_FIELDS = {
+    "quote": "Quote",
+    "author": "Author"
+}
 # activate the pipeline to transfer data to google sheets
 ITEM_PIPELINES = {
     GoogleSheetsPipeline: 300
